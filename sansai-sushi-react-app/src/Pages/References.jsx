84cd388d1b90f1fbf8af0238420459008e188a73
Elijah Aristide <139595440+Random-Eli@users.noreply.github.com>
2025-01-26 20:33:32 -0500

    Created Essential Page Structure;
    Installed React Router Dom;
    
    Installed Bootstrap CSS Library & JS Library for components;
    
    Imported All necessary libraries;

---
+++ sansai-sushi-react-app/src/Pages/References.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function References(){
+
+}
+export default References
---
+++ sansai-sushi-react-app/src/Pages/Sourcing.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Sourcing(){
+
+}
+export default Sourcing
