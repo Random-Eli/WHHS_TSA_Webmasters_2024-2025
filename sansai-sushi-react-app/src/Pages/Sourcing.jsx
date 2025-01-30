f2c99fb1b273580cc706a4c155253f4a7674e111
motionlikewo <lloydpeyton48@gmail.com>
2025-01-30 08:33:39 -0500

    Create sourcing page, description, and add images

--- sansai-sushi-react-app/src/Pages/Sourcing.jsx
+++ sansai-sushi-react-app/src/Pages/Sourcing.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import './Sourcing.css'
 
 function Sourcing(){
+    return (
+    <>
+        <h1>OUR SOURCING & SUSTAINABILITY PRACTICES</h1>
 
+        <p>
+            Here at Sansai Sushi, we are deeply committed to sustainable sourcing and farm-to-table practices, 
+            inspired by the pristine mountains of Japan. We work closely with local farmers and artisans who share our 
+            passion for organic, eco-friendly methods. Our ingredients—ranging from fresh vegetables and seaweed to 
+            rice—are all sourced from small-scale, environmentally conscious farms nestled in Japan’s mountainous regions,
+            where traditional, sustainable farming practices are still honored. We take great care to minimize food waste
+            by composting all organic scraps and using biodegradable packaging for takeout. Every dish we serve reflects
+            our dedication to the planet and local communities, allowing us to share the rich flavors of Japan’s culinary
+            traditions while supporting a more sustainable future.
+        </p>
+
+        <img className = 'imageLeft' src = 'https://cdn.pixabay.com/photo/2014/10/07/13/48/mountain-477832_1280.jpg' />
+        <img className = 'imageRight'  src = 'https://cdn.pixabay.com/photo/2017/06/10/05/26/rice-terraces-2389023_1280.jpg' />
+    </>
+    )
 }
 export default Sourcing
