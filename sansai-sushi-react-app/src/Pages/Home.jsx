171e3c317b4c83cf4ecb27a66c4d67ff838b0978
Elijah Aristide <139595440+Random-Eli@users.noreply.github.com>
2025-01-31 01:22:01 -0500

    Installed fullpage.js
    initialized fullpage.js Instance

--- sansai-sushi-react-app/src/Pages/Home.jsx
+++ sansai-sushi-react-app/src/Pages/Home.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import fullpage from 'fullpage.js';
+import 'fullpage.js/dist/fullpage.css'; 
 
 function Home(){
+
+    useEffect(() =>{
+        const fullpageInstance = new fullpage('#fullpage', {
+            
+        })
+
+        console.log("full page.js initialized!")
+
+        return () =>{
+            fullpageInstance.destroy();
+            console.log("full page.js uninitialized!")
+        };
+
+    }, []);
+    
+    return(
+
+    );
 
 }
 export default Home
