7e42de6acc8a5cefeae489672063fb675de8d302
noahc7 <noahbug9706@gmail.com>
2025-01-31 23:18:44 -0500

    Finished Front Page

--- sansai-sushi-react-app/src/Pages/Home.jsx
+++ sansai-sushi-react-app/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ import Photo5 from '../assets/Sushi2.jpg';
 import Photo6 from '../assets/Sushi3.jpg';
 import Photo7 from '../assets/Sushi4.jpg';
 import Photo8 from '../assets/sushiwiththesause.jpg';
-
+import Photo9 from '../assets/Mountains.jpg';
 
 
 
@@ -27,9 +27,13 @@ function Home() {
             <div className="backgroundImage">
                 <div className="paddingtop1"></div>
                 <h2 className="headerMain"> Sansai Sushi</h2>
+                <div className="container w-100">
+
+                <img src={Photo9} className="img-fluid rounded" alt="Responsive image"></img>
+                </div>
                 <div className="container">
                   
-                    <div className="paddingtop"></div>
+                <div className="paddingtop2"></div>
                     <h2 className="header text-center"> Where Vegan Meets the Timeless Elegance of Japan...</h2>
                     <Swiper
                         slidesPerView={3}
@@ -53,14 +57,20 @@ function Home() {
                     </Swiper>
 
                 </div>
-                <div className="paddingbottom"></div>
-                <div class="container text-center">
+                <div className="paddingtop"></div>
+                <div class="container text-center ">
                     <div class="row">
                         <div className="col-sm-6 col-md-5 col-lg-6">
-                        <h2 className="header text-center"> About Us:</h2>
+                        <div className="paddingbottom"></div>
+                        <h2 className="header text-center"> 
+                            About Us:</h2>
                         </div>
 
-                        <div className="col-sm-6 col-md-5 col-lg-6">Blah Blah Blah Give Us</div>
+                        <div className="col-sm-6 col-md-5 col-lg-6">
+                        <h2 className="aboutText"> 
+                            In operation for over 30 years, Sansai Sushi serves authentic vegetarian sushi with a modern twist. Founded by the Lee family, we here at Sansai Sushi strive to share plant-based, cruelty-free sushi with the world. For three decades, our commitment to sourcing from local farmers and artisans has allowed us to provide a not only sustainable, but also delicious dining experience. From vegetarians, to sushi lovers, to anyone open to amazing cuisine, join us and experience the taste of Sansai!
+                            </h2>
+                            </div>
                     </div>
                 </div>
                 <div className="paddingbottom"></div>
