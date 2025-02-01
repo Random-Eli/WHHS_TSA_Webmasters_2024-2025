38df3e2283705f8b83cf61074cce4772d82792f4
Elijah Aristide <139595440+Random-Eli@users.noreply.github.com>
2025-01-31 22:17:59 -0500

    Finished Sourcing Page with scroll parallex effect using swiper js. Enabled autoplay. Listed all sustainability practices.

--- sansai-sushi-react-app/src/Pages/Sourcing.jsx
+++ sansai-sushi-react-app/src/Pages/Sourcing.jsx
@@ -1,75 +1,128 @@
-import React from "react";
-import './Sourcing.css'
-import { useEffect } from 'react';
-import fullpage from 'fullpage.js';
-import 'fullpage.js/dist/fullpage.css';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+
+import './Sourcing.css';
+
+// import required modules
+import { Parallax, Pagination, Navigation, Autoplay } from 'swiper/modules';
+
+import img1 from '../assets/harvesting1.jpg';
+
 
 
 function Sourcing() {
-
-    useEffect(() => {
-        new fullpage('#fullpage', {
-            autoScrolling: true,
-            navigation: true,
-        });
-
-        console.log("FullPage.js initialized!");
-
-        return () => {
-            fullpage.destroy('all'); 
-            console.log("FullPage.js uninitialized!");
-        };
-    }, []);
-
     return (
 
-        <div id="fullpage">
+        <>
 
-            <div class="section" id="section0">
-        
-                <video class="overlay-video" loop muted="true" playsinline data-autoplay>
-                    <source src='../assets/Videos/vid - Sustainability Slide 1.mp4' type="video/mp4"></source>
-                </video>
+            <Swiper
+                style={{
+                    '--swiper-navigation-color': '#fff',
+                    '--swiper-pagination-color': '#fff',
+                }}
+                speed={1500}
+                parallax={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                  }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Parallax, Pagination, Navigation, Autoplay]}
+                className="mySwiper"
+            >
 
-                <div class="layer">
-                    <h1>“Sansai (山菜): The Wild, Fresh Heart of Our Vegan Sushi”</h1>
-                </div>
 
-            </div>
+                <div
+                    slot="container-start"
+                    className="parallax-bg"
+                    style={{
+                        'backgroundImage': `url(${img1})`
+                    }}
+                    data-swiper-parallax="-23%"
+                ></div>
 
-            <div class="section" id="section1">
 
-                <div class="slide" id="slide1">
-            
-                    <div class="layer">
-                        <h1>The Essence of Japanese Culture</h1>
+                <SwiperSlide>
+                    <div className="title" data-swiper-parallax="-2000">
+                        <h1>“Sansai (山菜): The Wild, Fresh Heart of Our Vegan Sushi”</h1>
                     </div>
+                </SwiperSlide>c
 
-                </div>
 
-                <div class="slide" id="slide2">
-                    
-                    <div class="layer">
+                <SwiperSlide>
+                    <div className="title" data-swiper-parallax="-2000">
+                        <h1>"The Essence of Japanese Culture”</h1>
+                    </div>
+                    <div className="card">
+                        <div className="text" data-swiper-parallax="-800">
+                            <div className="card-body">
+                                Our deeply connected roots in Japanese culture extend beyond just the authentic flavors of our food. Here at Sansai Sushi, we strive to embody the respectful and friendly spirit of Japan—not just in our kitchen and restaurant, but in every interaction with our guests and partners.
+                                Our partnership with local Japanese farmers represents our commitment to quality, sustainability, and community. By sourcing the freshest vegetables and ingredients directly from those who cultivate them with care, we create a dining experience that honors tradition while supporting the people who make it possible. At Sansai Sushi, every meal is a celebration of connection, craftsmanship, and the shared love of authentic Japanese cuisine.
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="title" data-swiper-parallax="-2000">
                         <h1>"Mottainai (もったいない): The Art of Avoiding Waste"</h1>
                     </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="text" data-swiper-parallax="-800">
+                                <p>
+                                    Here at Sansai Sushi we believe in the Japanese philosophy known as Mottainai, expressing a deep sense of regret over any type of waste,whether that be food, water, or even energy waste.
 
-                </div>
+                                </p>
+                            </div>
 
-            </div>
+                            <div className="text" data-swiper-parallax="-200">
+                                <ul>
+                                    <li>All takeout packaging is made from plant based fibers which are full bio degradable, allowing us to help reduce landfill waste.</li>
+                                    <li>The farmers that we partner with also believe in sustainability and conservation practices.Our farmers utilize techniques in order to get us the freshest ingredients, at the lowest environmental cost</li>
+                                    <li>We believe in re-purposing, just as the Japanese did in the Shojin Ryori period when Japan was a nearly zero waster society. Our menu is formulated with sustainability in mind, nearly all food scraps can be reused in either a dish, or even an original sauce.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="title" data-swiper-parallax="-2000">
+                        <h1>"Award Winning"</h1>
+                    </div>
+                    <div className="card">
+                        <div className="card-body" id = "lastCard">
+                            <div className="subtitle" data-swiper-parallax="-800">
+                                <p>Our efforts to provide delectable yet sustainable cuisine has not gone unnoticed either. Here are a couple of our achievements...</p>
+                            </div>
+
+                            <div className="text" data-swiper-parallax="-200">
+                                <ul>
+                                    <li>Three Michelin Star</li>
+                                    <li>LEED Certified</li>
+                                    <li>Green Restaurant Association Certified</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
 
 
-            <div class="section" id="section2">
+            </Swiper>
 
-                <div class="layer">
-                    <h1>Award Winning</h1>
-                </div>
-
-            </div>
-
-
-        </div>
-    );
-
+        </>
+    )
 }
 
 export default Sourcing
